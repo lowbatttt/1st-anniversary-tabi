0f806f03fc8b05ac363fff086727918430a4c202
lowbatttt <low.battttt@gmail.com>
2025-11-06 19:39:44 +0700

    Add files via upload

---
+++ file/functions.js
@@ -0,0 +1,80 @@
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function() {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function($) {
+	$.fn.typewriter = function() {
+		this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function timeElapse(date){
+	var current = Date();
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	seconds = seconds % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
+	$("#clock").html(result);
+
+	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
+	$("#message-box").html(text);
+
+}
+// =======================
+// FUNGSI UNTUK EFEK KETIK
+// =======================
+function typeText(id, lines, speed, delay) {
+    return new Promise(async (resolve) => {
+        const element = document.getElementById(id);
+        element.innerHTML = ""; // Kosongkan dulu area teks
+
+        for (let line of lines) {
+            for (let char of line) {
+                element.innerHTML += char;
+                await new Promise(r => setTimeout(r, speed)); // jeda per huruf
+            }
+            element.innerHTML += "<br>";
+            await new Promise(r => setTimeout(r, delay)); // jeda antar baris
+        }
+
+        resolve(); // Menandakan proses ketik sudah selesai
+    });
+}
